ed892bd9744fd7c4813ae758a839c4fbced7bead
bears <bears@bears.zone>
2022-09-16 16:32:05 -0500

    create card from id api

---
+++ src/routes/api/cards/fromId/+server.ts
@@ -0,0 +1,15 @@
+import redis from 'redis'
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET({ url }) {
+    const cardId = String(url.searchParams.get('cardId') ?? '');
+
+    const client = await redis.createClient({})
+    await client.connect()
+
+    const cardData = await client.json.GET(cardId)
+
+    await client.quit()
+
+    return new Response(JSON.stringify(cardData));
+}
