cd8b6f9dd534beb94887132afd0469b600573ce7
bears <bears@bears.zone>
2022-09-16 16:32:53 -0500

    fetch less data with readCollections endpoint

--- src/routes/api/collections/readCollections/+server.ts
+++ src/routes/api/collections/readCollections/+server.ts
@@ -1,11 +1,15 @@
-import { error } from '@sveltejs/kit';
 import prisma from '../../../../lib/db'
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ url }) {
   const email = String(url.searchParams.get('email') ?? 'na');
 
-  const foundCollections = await prisma.collection.findMany({ where: { user: { email: email } } })
-  // console.log(foundCollections)
+  const foundCollections = await prisma.collection.findMany({
+    where: { user: { email: email } }, select: {
+      id: true,
+      name: true
+    }
+  })
+
   return new Response(JSON.stringify(foundCollections));
 }
